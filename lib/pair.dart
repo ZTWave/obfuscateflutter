8f377c9dc21f0cf49aa0a57ec9483a8ea4e600f9
weizhiqiang <weizhiqiang@daikunkeji.com>
2025-01-10 18:02:03 +0800

    fix main import issue

--- lib/pair.dart
+++ lib/pair.dart
@@ -16,7 +16,14 @@ List<Pair<String, String>> toPairList(
 ) {
   List<Pair<String, String>> result = List.empty(growable: true);
   for (int i = 0; i < a.length; i++) {
-    result.add(Pair(a[i], b.elementAtOrNull(i) ?? ''));
+    final fileNameOrigin = a[i];
+    var fileNameOb = b.elementAtOrNull(i) ?? '';
+
+    if (fileNameOrigin == 'main') {
+      fileNameOb = 'main';
+    }
+
+    result.add(Pair(fileNameOrigin, fileNameOb));
   }
   return result;
 }
